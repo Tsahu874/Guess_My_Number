9a6562069d28ca07ce396426fdb91122702c2484
Tanishka Sahu <140744568+Tsahu874@users.noreply.github.com>
2023-11-16 00:36:41 +0530

    Add files via upload

---
+++ Guess-My-Number-Boilerplate/script.js
@@ -0,0 +1,17 @@
+let secretNumber = Math.trunc(Math.random()*20)+1;
+
+console.log("secretNumber",secretNumber);
+
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function(message){
+  const check = document.querySelector('.message').textContent= message;
+  console.log("check",check);
+}
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log("guess",guess);
+})
+displayMessage("Start Guessing...");
